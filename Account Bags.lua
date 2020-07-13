281973fea48d304386cba59d662e0d54fcebf45a
gburrows <george.burrows@ni.com>
2020-07-13 06:38:34 -0500

    Initial file load

---
+++ Account Bags.lua
@@ -0,0 +1,372 @@
+driverAccuracy={}
+woodAccuracy={}
+longironAccuracy={}
+shortironAccuracy={}
+wedgeAccuracy={}
+roughironAccuracy={}
+sandwedgeAccuracy={}
+driverAccuracy["Rocket"]={50,70,70,76,76,83,83,83,83,90}
+driverAccuracy["Extra Mile"]={0,7,7,7,29,29,45,45}
+driverAccuracy["Big Topper"]={10,0,15,15,24,41}
+driverAccuracy["Quarterback"]={73,80,80,80,100,100,100,100,100,100}
+driverAccuracy["The Rock"]={65,65,86,86,94,94,100,100}
+driverAccuracy["Thor's Hammer"]={50,64,64,64,64,64,92}
+driverAccuracy["Apocalypse"]={35,35,43,66,75,75,84}
+woodAccuracy["Horizon"]={0,0,12,12,31}
+woodAccuracy["Viper"]={20,20,35,40,51,51,56,56,67,72}
+woodAccuracy["Big Dawg"]={20,20,25,25,25,25,44,56}
+woodAccuracy["Hammerhead"]={65,65,83,95,95,95,100}
+woodAccuracy["Guardian"]={50,50,66,66,78,78,90,90}
+woodAccuracy["Sniper"]={100,100,100,100,100,100,100,100,100,100}
+woodAccuracy["Cataclysm"]={30,47,47,47,60,60,80}
+longironAccuracy["Grim Reaper"]={30,30,42,60,60,81}
+longironAccuracy["Backbone"]={30,40,40,50,66,66,76,76,82,92}
+longironAccuracy["Goliath"]={0,0,16,28,28,34,52,52}
+longironAccuracy["Saturn"]={20,20,35,35,35,51,51,51,56,56}
+longironAccuracy["B52"]={62,73,79,79,98,98,100}
+longironAccuracy["Grizzly"]={73,73,73,73,90,96,96,96}
+longironAccuracy["Tsunami"]={23,23,29,41,60,60,81}
+shortironAccuracy["Apache"]={0,4,17,17,27,27,32,32}
+shortironAccuracy["Kingfisher"]={65,69,83,88}
+shortironAccuracy["Runner"]={20,24,24,24,36,45,53,53,66,66}
+shortironAccuracy["Thorn"]={30,43,43,43,52,52,57,57}
+shortironAccuracy["Hornet"]={78,78,91,91,91,96,96,96}
+shortironAccuracy["Claw"]={25,29,41,41,50,50,54,54,66,75}
+shortironAccuracy["Falcon"]={45,45,54,59,59,75,75}
+wedgeAccuracy["Dart"]={10,22,22,30,30,43,43,47,60,64}
+wedgeAccuracy["Firefly"]={30,30,39,39,44,61}
+wedgeAccuracy["Boomerrang"]={0,0,9,9,25,25}
+wedgeAccuracy["Down-In-One"]={80,80,89,89,100,100,100,100}
+wedgeAccuracy["Skewer"]={25,29,29,41,41,50,50,58,58,71}
+wedgeAccuracy["Endbringer"]={70,70,79,79,95,95,100}
+wedgeAccuracy["Rapier"]={45,54,58,58,63,72,87,87}
+roughironAccuracy["Roughcutter"]={70,70,84,84,100,100,100,100}
+roughironAccuracy["Junglist"]={10,10,25,25,25}
+roughironAccuracy["Machete"]={20,20,40,53,60,60,80,80,94,94}
+roughironAccuracy["Off Roader"]={40,40,63,79,96}
+roughironAccuracy["Razor"]={100,100,100,100,100,100,100,100}
+roughironAccuracy["Amazon"]={20,20,35,35,60,69,87}
+roughironAccuracy["Nirvana"]={60,60,74,74,97,97,100,100}
+sandwedgeAccuracy["Castaway"]={5,19,19,27}
+sandwedgeAccuracy["Desert Storm"]={0,20,20,33,33,40,40,54,54}
+sandwedgeAccuracy["Malibu"]={60,81,81,88,88,96,96,100}
+sandwedgeAccuracy["Sahara"]={15,15,15,31,31,57,57}
+sandwedgeAccuracy["Sand Lizard"]={70,70,90,90,97,97,97,97,97,97}
+sandwedgeAccuracy["Houdini"]={0,0,7,14,37,52,52,69}
+sandwedgeAccuracy["Spitfire"]={50,50,65,65,90,100}
+
+driverPower={}
+woodPower={}
+longironPower={}
+shortironPower={}
+wedgePower={}
+roughironPower={}
+sandwedgePower={}
+driverPower["Rocket"]={190,190,192,198,198,202,204,204,206,211}
+driverPower["Extra Mile"]={220,220,224,226,226,234,234,236}
+driverPower["Big Topper"]={205,209,209,217,217,219}
+driverPower["Quarterback"]={197,197,203,203,207,207,214,214,218,218}
+driverPower["The Rock"]={210,210,210,217,217,221,221,226}
+driverPower["Thor's Hammer"]={220,220,222,229,232,232,232}
+driverPower["Apocalypse"]={229,231,231,234,240,240,240}
+woodPower["Horizon"]={170,171,173,173,173}
+woodPower["Viper"]={160,164,165,165,166,166,170,170,170,170}
+woodPower["Big Dawg"]={174,174,177,177,177,180,180,180}
+woodPower["Hammerhead"]={169,170,170,170,170,175,175}
+woodPower["Guardian"]={166,166,166,171,171,174,175,179}
+woodPower["Sniper"]={160,160,160,160,164,164,166,166,170,172}
+woodPower["Cataclysm"]={175,176,180,180,180,180,180}
+longironPower["Grim Reaper"]={115,115,115,115,115,116}
+longironPower["Backbone"]={118,118,119,119,119,121,121,123,127,127}
+longironPower["Goliath"]={130,132,132,132,135,135,135,135}
+longironPower["Saturn"]={115,115,115,119,119,119,120,120,124,124}
+longironPower["B52"]={127,127,132,135,135,135,135}
+longironPower["Grizzly"]={122,122,122,122,122,126,126,126}
+longironPower["Tsunami"]={127,129,133,133,134,134,135}
+shortironPower["Apache"]={84,84,86,89,89,90,90,90}
+shortironPower["Kingfisher"]={75,75,75,75}
+shortironPower["Runner"]={75,75,75,78,80,80,81,82,82,82}
+shortironPower["Thorn"]={81,81,83,86,86,89,89,90}
+shortironPower["Hornet"]={79,82,84,84,86,86,88,88}
+shortironPower["Claw"]={76,76,77,77,80,82,82,86,86,87}
+shortironPower["Falcon"]={81,82,82,85,85,88,88}
+wedgePower["Dart"]={35,35,37,37,39,39,39,41,41,41}
+wedgePower["Firefly"]={35,35,37,37,37,38}
+wedgePower["Boomerrang"]={39,41,41,42,42,44}
+wedgePower["Down-In-One"]={41,43,44,44,44,44,44,44}
+wedgePower["Skewer"]={36,36,39,39,39,39,40,42,42,42}
+wedgePower["Endbringer"]={39,41,41,43,43,44,44}
+wedgePower["Rapier"]={38,38,38,38,40,40,42,42}
+roughironPower["Roughcutter"]={75,78,78,88,88,92,92,103}
+roughironPower["Junglist"]={97,107,107,111,111}
+roughironPower["Machete"]={82,82,85,85,91,91,98,98,101,101}
+roughironPower["Off Roader"]={108,111,111,115,131}
+roughironPower["Razor"]={97,104,104,107,107,111,121,129}
+roughironPower["Amazon"]={121,131,135,135,135,135,135}
+roughironPower["Nirvana"]={112,112,112,123,123,129,129,135}
+sandwedgePower["Castaway"]={84,84,95,95}
+sandwedgePower["Desert Storm"]={64,73,80,80,80,80,90,90,96}
+sandwedgePower["Malibu"]={84,84,94,94,101,101,109,109}
+sandwedgePower["Sahara"]={96,96,107,107,115,115,120}
+sandwedgePower["Sand Lizard"]={68,68,74,74,80,91,91,100,100,100}
+sandwedgePower["Houdini"]={80,80,80,91,91,91,98,98}
+sandwedgePower["Spitfire"]={120,120,120,120,120,120}
+
+Account = {}
+Account["Main"] = {}
+Account["Main"][1] = {}
+Account["Main"][2] = {}
+Account["Main"][3] = {}
+Account["Main"][4] = {}
+Account["Main"][5] = {}
+Account["Alt 1"] = {}
+Account["Alt 1"][1] = {}
+Account["Alt 1"][2] = {}
+Account["Alt 1"][3] = {}
+Account["Alt 1"][4] = {}
+Account["Alt 1"][5] = {}
+Account["Alt 2"] = {}
+Account["Alt 2"][1] = {}
+Account["Alt 2"][2] = {}
+Account["Alt 2"][3] = {}
+Account["Alt 2"][4] = {}
+Account["Alt 2"][5] = {}
+
+-- Main Account, bag 1
+Account["Main"][1]["Driver"] = {"Apocalypse", 3}
+Account["Main"][1]["Wood"] = {"Sniper", 10}
+Account["Main"][1]["Long Iron"] = {"Grizzly", 6}
+Account["Main"][1]["Short Iron"] = {"Hornet", 6}
+Account["Main"][1]["Wedge"] = {"Rapier", 6}
+Account["Main"][1]["Rough Iron"] = {"Razor", 6}
+Account["Main"][1]["Sand Wedge"] = {"Malibu", 7}
+
+-- Main Account, bag 2
+Account["Main"][2]["Driver"] = {"Quarterback", 10}
+Account["Main"][2]["Wood"] = {"Sniper", 10}
+Account["Main"][2]["Long Iron"] = {"Grizzly", 6}
+Account["Main"][2]["Short Iron"] = {"Hornet", 6}
+Account["Main"][2]["Wedge"] = {"Rapier", 6}
+Account["Main"][2]["Rough Iron"] = {"Razor", 6}
+Account["Main"][2]["Sand Wedge"] = {"Malibu", 7}
+
+-- Main Account, bag 3
+Account["Main"][3]["Driver"] = {"Apocalypse", 3}
+Account["Main"][3]["Wood"] = {"Big Dawg", 7}
+Account["Main"][3]["Long Iron"] = {"Grizzly", 6}
+Account["Main"][3]["Short Iron"] = {"Hornet", 6}
+Account["Main"][3]["Wedge"] = {"Rapier", 6}
+Account["Main"][3]["Rough Iron"] = {"Razor", 6}
+Account["Main"][3]["Sand Wedge"] = {"Malibu", 7}
+
+-- Main Account, bag 4
+Account["Main"][4]["Driver"] = {"Quarterback", 10}
+Account["Main"][4]["Wood"] = {"Big Dawg", 7}
+Account["Main"][4]["Long Iron"] = {"Grizzly", 6}
+Account["Main"][4]["Short Iron"] = {"Hornet", 6}
+Account["Main"][4]["Wedge"] = {"Rapier", 6}
+Account["Main"][4]["Rough Iron"] = {"Razor", 6}
+Account["Main"][4]["Sand Wedge"] = {"Malibu", 7}
+
+-- Main Account, bag 5
+Account["Main"][5]["Driver"] = {"Quarterback", 10}
+Account["Main"][5]["Wood"] = {"Sniper", 10}
+Account["Main"][5]["Long Iron"] = {"Grizzly", 6}
+Account["Main"][5]["Short Iron"] = {"Runner", 10}
+Account["Main"][5]["Wedge"] = {"Rapier", 6}
+Account["Main"][5]["Rough Iron"] = {"Razor", 6}
+Account["Main"][5]["Sand Wedge"] = {"Malibu", 7}
+
+-- Alt 1 Account, bag 1
+Account["Alt 1"][1]["Driver"] = {"Extra Mile", 8}
+Account["Alt 1"][1]["Wood"] = {"Viper", 10}
+Account["Alt 1"][1]["Long Iron"] = {"Backbone", 10}
+Account["Alt 1"][1]["Short Iron"] = {"Kingfisher", 4}
+Account["Alt 1"][1]["Wedge"] = {"Firefly", 4}
+Account["Alt 1"][1]["Rough Iron"] = {"Roughcutter", 8}
+Account["Alt 1"][1]["Sand Wedge"] = {"Malibu", 6}
+
+-- Alt 1 Account, bag 2
+Account["Alt 1"][2]["Driver"] = {"Extra Mile", 8}
+Account["Alt 1"][2]["Wood"] = {"Big Dawg", 6}
+Account["Alt 1"][2]["Long Iron"] = {"Backbone", 10}
+Account["Alt 1"][2]["Short Iron"] = {"Kingfisher", 4}
+Account["Alt 1"][2]["Wedge"] = {"Firefly", 4}
+Account["Alt 1"][2]["Rough Iron"] = {"Roughcutter", 8}
+Account["Alt 1"][2]["Sand Wedge"] = {"Malibu", 6}
+
+-- Alt 1 Account, bag 3
+Account["Alt 1"][3]["Driver"] = {"Quarterback", 8}
+Account["Alt 1"][3]["Wood"] = {"Viper", 10}
+Account["Alt 1"][3]["Long Iron"] = {"Backbone", 10}
+Account["Alt 1"][3]["Short Iron"] = {"Kingfisher", 4}
+Account["Alt 1"][3]["Wedge"] = {"Firefly", 4}
+Account["Alt 1"][3]["Rough Iron"] = {"Roughcutter", 8}
+Account["Alt 1"][3]["Sand Wedge"] = {"Malibu", 6}
+
+-- Alt 1 Account, bag 4
+Account["Alt 1"][4]["Driver"] = {"Quarterback", 8}
+Account["Alt 1"][4]["Wood"] = {"Big Dawg", 6}
+Account["Alt 1"][4]["Long Iron"] = {"Backbone", 10}
+Account["Alt 1"][4]["Short Iron"] = {"Kingfisher", 4}
+Account["Alt 1"][4]["Wedge"] = {"Firefly", 4}
+Account["Alt 1"][4]["Rough Iron"] = {"Roughcutter", 8}
+Account["Alt 1"][4]["Sand Wedge"] = {"Malibu", 6}
+
+-- Alt 1 Account, bag 5
+Account["Alt 1"][5]["Driver"] = {"Rocket", 1}
+Account["Alt 1"][5]["Wood"] = {"Horizon", 1}
+Account["Alt 1"][5]["Long Iron"] = {"Backbone", 10}
+Account["Alt 1"][5]["Short Iron"] = {"Kingfisher", 4}
+Account["Alt 1"][5]["Wedge"] = {"Firefly", 4}
+Account["Alt 1"][5]["Rough Iron"] = {"Roughcutter", 8}
+Account["Alt 1"][5]["Sand Wedge"] = {"Malibu", 6}
+
+-- Alt 2 Account, bag 1
+Account["Alt 2"][1]["Driver"] = {"Rocket", 10}
+Account["Alt 2"][1]["Wood"] = {"Viper", 7}
+Account["Alt 2"][1]["Long Iron"] = {"Backbone", 6}
+Account["Alt 2"][1]["Short Iron"] = {"Kingfisher", 3}
+Account["Alt 2"][1]["Wedge"] = {"Dart", 10}
+Account["Alt 2"][1]["Rough Iron"] = {"Roughcutter", 7}
+Account["Alt 2"][1]["Sand Wedge"] = {"Malibu", 4}
+
+-- Alt 2 Account, bag 2
+Account["Alt 2"][2]["Driver"] = {"Rocket", 10}
+Account["Alt 2"][2]["Wood"] = {"Big Dawg", 4}
+Account["Alt 2"][2]["Long Iron"] = {"Backbone", 6}
+Account["Alt 2"][2]["Short Iron"] = {"Kingfisher", 3}
+Account["Alt 2"][2]["Wedge"] = {"Dart", 10}
+Account["Alt 2"][2]["Rough Iron"] = {"Roughcutter", 7}
+Account["Alt 2"][2]["Sand Wedge"] = {"Malibu", 4}
+
+-- Alt 2 Account, bag 3
+Account["Alt 2"][3]["Driver"] = {"Extra Mile", 5}
+Account["Alt 2"][3]["Wood"] = {"Viper", 7}
+Account["Alt 2"][3]["Long Iron"] = {"Backbone", 6}
+Account["Alt 2"][3]["Short Iron"] = {"Kingfisher", 3}
+Account["Alt 2"][3]["Wedge"] = {"Dart", 10}
+Account["Alt 2"][3]["Rough Iron"] = {"Roughcutter", 1}
+Account["Alt 2"][3]["Sand Wedge"] = {"Malibu", 4}
+
+-- Alt 2 Account, bag 4
+Account["Alt 2"][4]["Driver"] = {"Extra Mile", 5}
+Account["Alt 2"][4]["Wood"] = {"Big Dawg", 4}
+Account["Alt 2"][4]["Long Iron"] = {"Backbone", 6}
+Account["Alt 2"][4]["Short Iron"] = {"Kingfisher", 3}
+Account["Alt 2"][4]["Wedge"] = {"Dart", 10}
+Account["Alt 2"][4]["Rough Iron"] = {"Roughcutter", 7}
+Account["Alt 2"][4]["Sand Wedge"] = {"Malibu", 4}
+
+-- Alt 2 Account, bag 5
+Account["Alt 2"][5]["Driver"] = {"Rocket", 10}
+Account["Alt 2"][5]["Wood"] = {"Viper", 7}
+Account["Alt 2"][5]["Long Iron"] = {"Backbone", 6}
+Account["Alt 2"][5]["Short Iron"] = {"Kingfisher", 3}
+Account["Alt 2"][5]["Wedge"] = {"Dart", 10}
+Account["Alt 2"][5]["Rough Iron"] = {"Roughcutter", 7}
+Account["Alt 2"][5]["Sand Wedge"] = {"Malibu", 4}
+
+-- Club Background Pairs
+ClubMeter={}
+ClubMeter["Rocket"]="Driver_Rocket"
+ClubMeter["Extra Mile"]="Driver_Extra_Mile"
+ClubMeter["Big Topper"]="Driver_Big_Topper"
+ClubMeter["Quarterback"]="Driver_Quarterback"
+ClubMeter["The Rock"]="Driver_Rock"
+ClubMeter["Thor's Hammer"]="Driver_Thors_Hammer"
+ClubMeter["Apocalypse"]="Driver_Apocalypse"
+ClubMeter["Horizon"]="Wood_Horizon"
+ClubMeter["Viper"]="Wood_Viper"
+ClubMeter["Big Dawg"]="Wood_Big_Dawg"
+ClubMeter["Hammerhead"]="Wood_Hammerhead"
+ClubMeter["Guardian"]="Wood_Guardian"
+ClubMeter["Sniper"]="Wood_Sniper"
+ClubMeter["Cataclysm"]="Wood_Cataclysm"
+ClubMeter["Grim Reaper"]="LongIron_Grim_Reaper"
+ClubMeter["Backbone"]="LongIron_Backbone"
+ClubMeter["Goliath"]="LongIron_Goliath"
+ClubMeter["Saturn"]="LongIron_Saturn"
+ClubMeter["B52"]="LongIron_B52"
+ClubMeter["Grizzly"]="LongIron_Grizzly"
+ClubMeter["Tsunami"]="LongIron_Tsunami"
+ClubMeter["Apache"]="ShortIron_Apache"
+ClubMeter["Kingfisher"]="ShortIron_Kingfisher"
+ClubMeter["Runner"]="ShortIron_Runner"
+ClubMeter["Thorn"]="ShortIron_Thorn"
+ClubMeter["Hornet"]="ShortIron_Hornet"
+ClubMeter["Claw"]="ShortIron_Claw"
+ClubMeter["Falcon"]="ShortIron_Falcon"
+ClubMeter["Dart"]="Wedge_Dart"
+ClubMeter["Firefly"]="Wedge_Firefly"
+ClubMeter["Boomerrang"]="Wedge_Boomerang"
+ClubMeter["Down-In-One"]="Wedge_DownInOne"
+ClubMeter["Skewer"]="Wedge_Skewer"
+ClubMeter["Endbringer"]="Wedge_EndBringer"
+ClubMeter["Rapier"]="Wedge_Rapier"
+ClubMeter["Roughcutter"]="RoughIron_Roughcutter"
+ClubMeter["Junglist"]="RoughIron_Junglist"
+ClubMeter["Machete"]="RoughIron_Machete"
+ClubMeter["Off Roader"]="RoughIron_Off_Roader"
+ClubMeter["Razor"]="RoughIron_Razor"
+ClubMeter["Amazon"]="RoughIron_Amazon"
+ClubMeter["Nirvana"]="RoughIron_Nirvana"
+ClubMeter["Castaway"]="SandWedge_Castaway"
+ClubMeter["Desert Storm"]="SandWedge_Desert_Storm"
+ClubMeter["Malibu"]="SandWedge_Malibu"
+ClubMeter["Sahara"]="SandWedge_Sahara"
+ClubMeter["Sand Lizard"]="SandWedge_Sand_Lizard"
+ClubMeter["Houdini"]="SandWedge_Houdini"
+ClubMeter["Spitfire"]="SandWedge_Spitfire"
+
+
+function UpdateClubs()
+    -- Gather the account & bag details
+    acct = SKIN:GetVariable('Account','n/a')
+    bag = tonumber(SKIN:GetVariable('Bag','n/a'))
+    SKIN:Bang('!HideMeterGroup', 'ClubIcons')
+    -- Driver values
+    name = Account[acct][bag]["Driver"][1]
+    lvl = Account[acct][bag]["Driver"][2]
+    SKIN:Bang('!SetVariable', 'DriverMax', driverPower[name][lvl])
+    SKIN:Bang('!SetVariable', 'DriverAcc', driverAccuracy[name][lvl])
+    SKIN:Bang('!ShowMeter', ClubMeter[name])
+    -- Wood values
+    name = Account[acct][bag]["Wood"][1]
+    lvl = Account[acct][bag]["Wood"][2]
+    SKIN:Bang('!SetVariable', 'WoodMax', woodPower[name][lvl])
+    SKIN:Bang('!SetVariable', 'WoodAcc', woodAccuracy[name][lvl])
+    SKIN:Bang('!ShowMeter', ClubMeter[name])
+    -- Long Iron values
+    name = Account[acct][bag]["Long Iron"][1]
+    lvl = Account[acct][bag]["Long Iron"][2]
+    SKIN:Bang('!SetVariable', 'LongIronMax', longironPower[name][lvl])
+    SKIN:Bang('!SetVariable', 'LongIronAcc', longironAccuracy[name][lvl])
+    SKIN:Bang('!ShowMeter', ClubMeter[name])
+    -- Short Iron values
+    name = Account[acct][bag]["Short Iron"][1]
+    lvl = Account[acct][bag]["Short Iron"][2]
+    SKIN:Bang('!SetVariable', 'ShortIronMax', shortironPower[name][lvl])
+    SKIN:Bang('!SetVariable', 'ShortIronAcc', shortironAccuracy[name][lvl])
+    SKIN:Bang('!ShowMeter', ClubMeter[name])
+    -- Wedge values
+    name = Account[acct][bag]["Wedge"][1]
+    lvl = Account[acct][bag]["Wedge"][2]
+    SKIN:Bang('!SetVariable', 'WedgeMax', wedgePower[name][lvl])
+    SKIN:Bang('!SetVariable', 'WedgeAcc', wedgeAccuracy[name][lvl])
+    SKIN:Bang('!ShowMeter', ClubMeter[name])
+    -- Rough Iron values
+    name = Account[acct][bag]["Rough Iron"][1]
+    lvl = Account[acct][bag]["Rough Iron"][2]
+    SKIN:Bang('!SetVariable', 'RoughIronMax', roughironPower[name][lvl])
+    SKIN:Bang('!SetVariable', 'RoughIronAcc', roughironAccuracy[name][lvl])
+    SKIN:Bang('!ShowMeter', ClubMeter[name])
+    -- Sand Wedge values
+    name = Account[acct][bag]["Sand Wedge"][1]
+    lvl = Account[acct][bag]["Sand Wedge"][2]
+    SKIN:Bang('!SetVariable', 'SandWedgeMax', sandwedgePower[name][lvl])
+    SKIN:Bang('!SetVariable', 'SandWedgeAcc', sandwedgeAccuracy[name][lvl])
+    SKIN:Bang('!ShowMeter', ClubMeter[name])
+    
+end
+
